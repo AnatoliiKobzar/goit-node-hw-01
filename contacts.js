001ef3d435535439a1000fe6794e41817dcc3150
KobzarAnatolii <TohaPirat@gmail.com>
2023-05-05 16:23:53 +0300

    rename folder

--- contacts.js
+++ contacts.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 const { nanoid } = require('nanoid');
 
-const contactsPath = path.resolve('bd', 'contacts.json');
+const contactsPath = path.resolve('db', 'contacts.json');
 
 const updateContacts = async contacts => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
